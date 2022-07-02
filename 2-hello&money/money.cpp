87d1ea949ca5dc8f1c2509c18879936852fd09a5
Ashish Sharma <80460443+Ashish2824@users.noreply.github.com>
2022-07-02 13:50:55 +0530

    Add files via upload

---
+++ 2-hello&money/money.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int amount1;
+    cout<<"First amount is -> ";
+    cin>>amount1;
+
+    int amount2;
+    cout<<"Second amount is -> ";
+    cin>>amount2;
+
+    int sum = amount1+amount2;
+    cout<<"TOTAL AMOUNT = "<<sum;
+
+    return 0;
+}
