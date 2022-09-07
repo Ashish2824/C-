4666b2d1a362483e975a17a9e7fb8d2cc3ed30c1
Ashish Sharma <80460443+Ashish2824@users.noreply.github.com>
2022-09-07 18:34:27 +0530

    Add files via upload

---
+++ a)Extra/PrimeNum.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+#include<math.h>        //<c.math>
+
+int main(){
+
+    int n;
+    cout<<"Enter the Number -> ";
+    cin>>n;
+
+    bool flag = 0;
+
+    for(int i=2; i<sqrt(n); i++){
+        if(n%i==0){
+            cout<<"Entered Number is NON-PRIME"<<endl;
+            flag=1;
+            break;
+        }
+    }
+    if(flag==0){
+        cout<<"Entered Number is PRIME"<<endl;
+    }
+
+    return 0;
+}
